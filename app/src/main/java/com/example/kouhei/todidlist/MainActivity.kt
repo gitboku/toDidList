5f33f023bf5d9d8f5d4f01c0802043013554ba44
Kouhei Sano <ratatatta6@gmail.com>
2018-02-04 00:52:33 +0900

    add fragment tab, but has error "android.view.InflateException: Binary XML file line #0: Binary XML file line #0: Error inflating class fragment"

--- app/src/main/java/com/example/kouhei/todidlist/MainActivity.kt
+++ app/src/main/java/com/example/kouhei/todidlist/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.kouhei.todidlist
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.app.FragmentActivity
+import android.util.AttributeSet
+import android.view.View
 
-class MainActivity : FragmentActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var tabLayout = findViewById<TabLayout>(R.id.tabs)
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_calender))
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_stack))
     }
 }
