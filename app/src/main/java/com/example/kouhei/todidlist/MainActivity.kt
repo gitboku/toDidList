34e3e803c7d00895822c4b27f53fffeb5236fefd
Kouhei Sano <ratatatta6@gmail.com>
2018-02-04 23:53:02 +0900

    TabApplicationの実装を丸コピした。 タブ機能が動いていることを確認した。

--- app/src/main/java/com/example/kouhei/todidlist/MainActivity.kt
+++ app/src/main/java/com/example/kouhei/todidlist/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.example.kouhei.todidlist
 
-import android.content.Context
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.TabLayout
 import android.support.v4.app.FragmentActivity
-import android.util.AttributeSet
-import android.view.View
+import android.support.v4.app.FragmentTabHost
 
-class MainActivity : AppCompatActivity() {
+class MainActivity :  FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
-        var tabLayout = findViewById<TabLayout>(R.id.tabs)
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_calender))
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_stack))
+        var mTabHost = findViewById<FragmentTabHost>(R.id.tabhost)
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.tabContent)
+
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab_calender").setIndicator(getString(R.string.tab_calender), null),
+                CalenderFragment::class.java,
+                null)
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab_stack").setIndicator(getString(R.string.tab_stack), null),
+                CalenderFragment::class.java,
+                null)
     }
 }
