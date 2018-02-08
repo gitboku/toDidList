820b8615ebead47efece267b73e375f837026709
Kouhei Sano <ratatatta6@gmail.com>
2018-02-08 10:11:40 +0900

    tabLayout is working ! app.gradleに入れたバージョンが競合？していた。

--- app/src/main/java/com/example/kouhei/todidlist/MainActivity.kt
+++ app/src/main/java/com/example/kouhei/todidlist/MainActivity.kt
@@ -1,26 +1,39 @@
 package com.example.kouhei.todidlist
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
-import android.support.v4.app.FragmentTabHost
+import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.AppCompatActivity
+import android.widget.FrameLayout
 
-class MainActivity :  FragmentActivity() {
+class MainActivity :  AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
-        var mTabHost = findViewById<FragmentTabHost>(R.id.tabhost)
-        mTabHost.setup(this, getSupportFragmentManager(), R.id.tabContent)
+        var tabLayout = findViewById<TabLayout>(R.id.tabs)
+        var frameLayout = findViewById<FrameLayout>(R.id.tabContent)
 
-        mTabHost.addTab(
-                mTabHost.newTabSpec("tab_calender").setIndicator(getString(R.string.tab_calender), null),
-                CalenderFragment::class.java,
-                null)
-        mTabHost.addTab(
-                mTabHost.newTabSpec("tab_stack").setIndicator(getString(R.string.tab_stack), null),
-                CalenderFragment::class.java,
-                null)
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_calender))
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_stack))
+
+//        tabLayout.setOnTabSelectedListener(object : TabLayout.OnTabSelectedListener() {
+//            fun onTabSelected(tab:TabLayout.Tab) {
+//                val fragment: Fragment = null
+//
+//                when (tab.position) {
+//                    0 -> fragment = CalenderFragment()
+//                    1 -> fragment = CalenderFragment()
+//                }
+//                val fm = getSupportFragmentManager()
+//                val ft = fm.beginTransaction()
+//                ft.replace(R.id.tabContent, fragment)
+//                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+//                ft.commit()
+//            }
+//        })
     }
 }
