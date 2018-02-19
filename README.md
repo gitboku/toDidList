584db16723bc220435a8b6fbaf67d2a1e62c2086
Kouhei Sano <ratatatta6@gmail.com>
2018-02-20 00:57:16 +0900

    ExtendedCalendarViewが動いた。 外部moduleはgitignoreに入れたので、ほかのPCにプロジェクトをインポートするときはGitHubからインポートする必要がある。 その旨はREADMEに記述した。 タブを外したが、タブの代わりにアプリバーにトグルを置くことで十分に代用が可能。

--- README.md
+++ README.md
@@ -7,3 +7,11 @@
 
 ↓MOCK
 ![todidlist_mock](https://user-images.githubusercontent.com/13359636/35805777-7a7ee332-0ac0-11e8-9a5f-b7e0bcffba38.png)
+
+
+このアプリは以下のmoduleを使用しています。
+外部moduleはgitignoreに含めているので、外部からインポートする必要があります。
+
+・ExtendedCalenderView
+　https://github.com/tyczj/ExtendedCalendarView
+　https://www.youtube.com/watch?v=zVX32JkuwMQ
--- app/src/main/java/com/example/kouhei/todidlist/MainActivity.kt
+++ app/src/main/java/com/example/kouhei/todidlist/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.CalendarView
 import android.widget.FrameLayout
 
 class MainActivity :  AppCompatActivity() {
@@ -12,30 +13,32 @@ class MainActivity :  AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var tabLayout = findViewById<TabLayout>(R.id.tabs)
-        var frameLayout = findViewById<FrameLayout>(R.id.tabContent)
+//        var tabLayout = findViewById<TabLayout>(R.id.tabs)
+//        var frameLayout = findViewById<FrameLayout>(R.id.tabContent)
 
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_calender))
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_stack))
+//        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_calender))
+//        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_stack))
+
+        var calender = findViewById<CalendarView>(R.id.calender)
 
         // setOnTabSelectedListener was deprecated in API level 24.1.0.
         // Use addOnTabSelectedListener(OnTabSelectedListener) and removeOnTabSelectedListener(OnTabSelectedListener).
         // TabLayout.OnTabSelectedListener ← タブの選択状態が変更されたときに呼び出されるcallback interface
         // "object:" ← 無名クラスを作るときに使う
-        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
-            // "onTabSelected","onTabUnselected","onTabReselected"の3つを実装しないとエラー
-
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                Log.i("myTag", "onTabSelebted")
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab) {
-                Log.i("myTag", "onTabUnselected")
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab) {
-                Log.i("myTag", "onTabReselected")
-            }
-        })
+//        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+//            // "onTabSelected","onTabUnselected","onTabReselected"の3つを実装しないとエラー
+//
+//            override fun onTabSelected(tab: TabLayout.Tab) {
+//                Log.i("myTag", "onTabSelebted")
+//            }
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab) {
+//                Log.i("myTag", "onTabUnselected")
+//            }
+//
+//            override fun onTabReselected(tab: TabLayout.Tab) {
+//                Log.i("myTag", "onTabReselected")
+//            }
+//        })
     }
 }
