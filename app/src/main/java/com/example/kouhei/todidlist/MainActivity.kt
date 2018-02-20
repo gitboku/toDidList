09a3dd702f9e38b684e9a56c1f84b87c7016a21a
Kouhei Sano <ratatatta6@gmail.com>
2018-02-20 23:27:29 +0900

    タブは使わないので消した

--- app/src/main/java/com/example/kouhei/todidlist/MainActivity.kt
+++ app/src/main/java/com/example/kouhei/todidlist/MainActivity.kt
@@ -13,32 +13,6 @@ class MainActivity :  AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        var tabLayout = findViewById<TabLayout>(R.id.tabs)
-//        var frameLayout = findViewById<FrameLayout>(R.id.tabContent)
-
-//        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_calender))
-//        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_stack))
-
         var calender = findViewById<CalendarView>(R.id.calender)
-
-        // setOnTabSelectedListener was deprecated in API level 24.1.0.
-        // Use addOnTabSelectedListener(OnTabSelectedListener) and removeOnTabSelectedListener(OnTabSelectedListener).
-        // TabLayout.OnTabSelectedListener ← タブの選択状態が変更されたときに呼び出されるcallback interface
-        // "object:" ← 無名クラスを作るときに使う
-//        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
-//            // "onTabSelected","onTabUnselected","onTabReselected"の3つを実装しないとエラー
-//
-//            override fun onTabSelected(tab: TabLayout.Tab) {
-//                Log.i("myTag", "onTabSelebted")
-//            }
-//
-//            override fun onTabUnselected(tab: TabLayout.Tab) {
-//                Log.i("myTag", "onTabUnselected")
-//            }
-//
-//            override fun onTabReselected(tab: TabLayout.Tab) {
-//                Log.i("myTag", "onTabReselected")
-//            }
-//        })
     }
 }
