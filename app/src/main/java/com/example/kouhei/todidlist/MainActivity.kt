62e8ceee3bb236e2b448daa25875c2b6fd837c21
Kouhei Sano <ratatatta6@gmail.com>
2018-02-21 22:24:26 +0900

    ExtendedCalendarView使わないほうがいい。

--- app/src/main/java/com/example/kouhei/todidlist/MainActivity.kt
+++ app/src/main/java/com/example/kouhei/todidlist/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.kouhei.todidlist
 
 import android.os.Bundle
-import android.support.design.widget.TabLayout
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.widget.CalendarView
-import android.widget.FrameLayout
 
 class MainActivity :  AppCompatActivity() {
 
@@ -13,6 +10,6 @@ class MainActivity :  AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var calender = findViewById<CalendarView>(R.id.calender)
+        var myCalendar = findViewById<CalendarView>(R.id.calendar)
     }
 }
