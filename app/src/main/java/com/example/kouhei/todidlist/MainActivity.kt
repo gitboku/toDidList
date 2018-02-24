07aa0fb77f34c357d1834dd6bede39f2694a16a7
Kouhei Sano <ratatatta6@gmail.com>
2018-02-24 19:22:24 +0900

    カレンダー画面で編集ボタンをタップしたときに呼び出すメソッドを作成。 まだ定義しただけで使用してない。

--- app/src/main/java/com/example/kouhei/todidlist/MainActivity.kt
+++ app/src/main/java/com/example/kouhei/todidlist/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.example.kouhei.todidlist
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.CalendarView
+import java.util.*
 
 class MainActivity :  AppCompatActivity() {
+
+    val EXTRA_DATE = "com.example.kouhei.todidList.SELECTED_DATE"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var myCalendar = findViewById<CalendarView>(R.id.calendar)
+    }
+
+    // 日付がタップされたときにEdit（または閲覧）画面に遷移する
+    // TODO: カレンダーのインスタンスを引数に含める
+    fun moveToEdit(view: View){
+        // 一つ目のコンストラクタはContext。ActivityはContextのサブクラスなのでthisを使う
+        // 二つ目はIntentが送られるアプリコンポーネントのClass（開始されるActivity）
+        var intent = Intent(this, EditDiaryActivity::class.java)
+
+        // TODO: カレンダー部分で選択してる日付を渡すようにする
+        intent.putExtra(EXTRA_DATE, Date())
+
+        startActivity(intent)
     }
 }
