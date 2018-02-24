ba76a05c7053507151bfae5122ccf78b3771dde5
Kouhei Sano <ratatatta6@gmail.com>
2018-02-24 19:27:00 +0900

    EditDiaryActivityをempty activityで作り直し。 間違えてbasic activityでnewしていた。

--- app/src/main/java/com/example/kouhei/todidlist/EditDiaryActivity.kt
+++ app/src/main/java/com/example/kouhei/todidlist/EditDiaryActivity.kt
@@ -1,22 +1,12 @@
 package com.example.kouhei.todidlist
 
-import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-
-import kotlinx.android.synthetic.main.activity_edit_diary.*
+import android.os.Bundle
 
 class EditDiaryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_diary)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
-
 }
