a83cf8d0f030397ccb1703f01f61a703495ce2ec
Kouhei Sano <ratatatta6@gmail.com>
2018-03-21 16:55:30 +0900

    timestampをDBに保存する用のDATEに変換するメソッドを作成

--- app/src/main/java/com/example/kouhei/todidlist/EditDiaryActivity.kt
+++ app/src/main/java/com/example/kouhei/todidlist/EditDiaryActivity.kt
@@ -3,6 +3,10 @@ package com.example.kouhei.todidlist
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edit_diary.*
 import android.arch.persistence.room.Room
+import java.text.SimpleDateFormat
+import java.util.*
+
+const val DATE_PATTERN_TO_DATABASE = "yyyyMMdd"
 
 class EditDiaryActivity : MyAppCompatActivity() {
 
@@ -15,6 +19,19 @@ class EditDiaryActivity : MyAppCompatActivity() {
         // kotlinではgetIntent()は"intent"でOK
         var selectedDate = intent.getLongExtra(EXTRA_DATE, 0)
 
-        diaryPanel.setText(selectedDate.toString())
+        diaryPanel.setText(getDateTime(selectedDate))
+    }
+
+    /**
+     * Long型で受け取ったタイムスタンプを日付に変更する
+     */
+    private fun getDateTime(timestamp: Long): String? {
+        try {
+            val sdf = SimpleDateFormat(DATE_PATTERN_TO_DATABASE)
+            val netDate = Date(timestamp)
+            return sdf.format(netDate)
+        } catch (e: Exception) {
+            return e.toString()
+        }
     }
 }
