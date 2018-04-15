67ce4f0f20312547b62411ff9bb17dccf0e78467
Kouhei Sano <ratatatta6@gmail.com>
2018-04-15 19:06:51 +0900

    AppDatabaseにSingleton（Double-chceck）を適用

--- app/src/main/java/com/example/kouhei/todidlist/AppDatabase.kt
+++ app/src/main/java/com/example/kouhei/todidlist/AppDatabase.kt
@@ -1,8 +1,6 @@
 package com.example.kouhei.todidlist
 
 import android.arch.persistence.room.*
-import com.example.kouhei.todidlist.Diary
-import com.example.kouhei.todidlist.DiaryDao
 
 @Database(entities = [Diary::class], version = 1) // Kotlin 1.2からは arrayOf(com.example.kouhei.todidlist.Diary::class)の代わりに[com.example.kouhei.todidlist.Diary::class]と書ける
 abstract class AppDatabase : RoomDatabase() {
@@ -10,4 +8,22 @@ abstract class AppDatabase : RoomDatabase() {
     // DAOを取得する。
     // この抽象クラスの実装はRoomライブラリのアノテーション処理で自動生成される
     abstract fun diaryDao(): DiaryDao
+
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: android.content.Context): AppDatabase? {
+            if (INSTANCE == null) {
+                synchronized(AppDatabase::class) {
+                    INSTANCE = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "applyDatabase").build()
+                }
+            }
+            return INSTANCE
+        }
+    }
+
+    fun destroyInstance() {
+        INSTANCE = null
+    }
 }
