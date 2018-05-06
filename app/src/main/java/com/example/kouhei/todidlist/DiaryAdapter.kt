ea372f04fd9e9487cd383ed44ad6141a0f3d318a
Kouhei Sano <ratatatta6@gmail.com>
2018-05-06 20:23:12 +0900

    ViewHolderクラスの宣言はややこしいので下のほうに移した。
    ViewHolderをクリックしたときに実行されるリスナーをセットした

--- app/src/main/java/com/example/kouhei/todidlist/DiaryAdapter.kt
+++ app/src/main/java/com/example/kouhei/todidlist/DiaryAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.kouhei.todidlist
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
@@ -8,15 +9,24 @@ import android.widget.TextView
 class DiaryAdapter(private val myDataset: ArrayList<String>) :
         RecyclerView.Adapter<DiaryAdapter.ViewHolder>() {
 
-    // RecyclerViewの一要素となるXML要素の型を引数に指定する
-    // この場合はdiary_list_item.xmlのTextView
-    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
-
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DiaryAdapter.ViewHolder {
         val textView = LayoutInflater.from(parent.context)
                 .inflate(R.layout.diary_list_item, parent, false) as TextView
-        return ViewHolder(textView)
+
+        val viewHolder = ViewHolder(textView)
+
+        textView.setOnClickListener {
+            val position = viewHolder.adapterPosition
+            Log.d("myTag", "this position is " + position)
+        }
+        /**
+         * １OK：ViewHolderをクリックしたとき実行されるリスナーをセットする
+         * ２：ViewHolderに日付情報を持たせる
+         * ３：Intentを作成する
+         * ４：IntentをmoveToAnotherPage()に渡す
+         */
+        return viewHolder
     }
 
     // Replace the contents of a view (invoked by the layout manager)
@@ -26,4 +36,8 @@ class DiaryAdapter(private val myDataset: ArrayList<String>) :
     }
 
     override fun getItemCount() = myDataset.size
+
+    // RecyclerViewの一要素となるXML要素の型を引数に指定する
+    // この場合はdiary_list_item.xmlのTextView
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 }
