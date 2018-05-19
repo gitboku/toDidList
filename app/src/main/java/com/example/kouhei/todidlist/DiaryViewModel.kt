bfa1920121f8c7afc5859b45367bceff9a65f3a2
Kouhei Sano <ratatatta6@gmail.com>
2018-05-19 19:42:57 +0900

    ViewModelの親クラスをAndroidViewModelに変更。

--- app/src/main/java/com/example/kouhei/todidlist/DiaryViewModel.kt
+++ app/src/main/java/com/example/kouhei/todidlist/DiaryViewModel.kt
@@ -1,15 +1,17 @@
 package com.example.kouhei.todidlist
 
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.ViewModel
 
 /**
- * UIコントローラーからデータに関するロジックを切り離すために作成される
+ * UIコントローラーからデータに関するロジックを切り離すために作成される。
+ * 原則として、ActivityやFragmentは「データをスクリーンに描く」のが役割。
  *
  * ViewModelProviderに渡されるライフサイクルにスコープされる。
  * つまり、Activityに渡されると、それがDestroyされるまで生きてる
  */
-class AllDiaryViewModel: ViewModel() {
+class DiaryViewModel(application: Application): AndroidViewModel(application) {
     private var mDiaryLiveData: LiveData<List<Diary>>? = null
     fun getAllDiaries(diaryDao: DiaryDao): LiveData<List<Diary>> {
         if (mDiaryLiveData == null) {
