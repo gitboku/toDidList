6150e6049ef9a2459f5238df8969ea915bc4ecb7
Kouhei Sano <ratatatta6@gmail.com>
2018-05-23 23:04:38 +0900

    タスクの内容はすべて別で管理することにしたので、コメントは消した。

--- app/src/main/java/com/example/kouhei/todidlist/DiaryAdapter.kt
+++ app/src/main/java/com/example/kouhei/todidlist/DiaryAdapter.kt
@@ -27,13 +27,7 @@ class DiaryAdapter(private val myDataset: ArrayList<String>) :
         textView.setOnClickListener {
             nowTimeStamp = mDiaries[viewHolder.adapterPosition].calendarDate
         }
-        /**
-         * ３：MainStackActivityからEditDiaryActivityに遷移できるようにする。
-         * 　　１：日付情報をViewHolderに持たせる
-         * 　　２：IntentをmoveToAnotherPage()に渡す
-         * ４：画像を保存できるようにする
-         * ５：MainActivityで画像を表示してる時だけstatus barとToolbarを透明にする
-         */
+
         return viewHolder
     }
 
