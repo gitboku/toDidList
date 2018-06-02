861f711393d095876b2d4b394a674aa9c8861903
Kouhei Sano <ratatatta6@gmail.com>
2018-06-03 01:37:03 +0900

    AndroidのGalleryから写真を選択して、それをEditPanelに表示できるようになった。
    まだsave機能はつけていない。

--- app/src/main/java/com/example/kouhei/todidlist/EditDiaryActivity.kt
+++ app/src/main/java/com/example/kouhei/todidlist/EditDiaryActivity.kt
@@ -3,9 +3,19 @@ package com.example.kouhei.todidlist
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edit_diary.*
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import android.os.Build
+import android.support.v4.app.ActivityCompat
 import android.view.Menu
 import android.view.MenuItem
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.graphics.drawable.BitmapDrawable
+import android.provider.MediaStore
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
+import java.io.IOException
+import java.nio.channels.GatheringByteChannel
 import kotlin.concurrent.thread
 
 class EditDiaryActivity : MyAppCompatActivity() {
@@ -13,6 +23,10 @@ class EditDiaryActivity : MyAppCompatActivity() {
     private var db: AppDatabase? = null
     private var nowTimeStamp: Long = 0
     private var selectDate: Int = 0
+
+    // パーミッションを求めるダイアログにユーザーが応答したとき、
+    // requestLocationPermission()が渡してonRequestPermissionsResult()が受け取る合言葉
+    private val READ_PERMISSION_REQUEST_CODE = 1000
 
     companion object {
         val MAIN_PAGE: String = MainActivity::class.java.simpleName
@@ -72,10 +86,108 @@ class EditDiaryActivity : MyAppCompatActivity() {
             true
         }
 
+        // 画像保存ボタン（このボタンを押しただけでは画像はsaveされない）
+        // 画像をsaveするのはR.id.action_saveが押されたとき。
+        R.id.action_image -> {
+            showPicturesFromExternalStorage()
+            true
+        }
+
         else -> {
             // If we got here, the user's action was not recognized.
             // Invoke the superclass to handle it.
             super.onOptionsItemSelected(item)
+        }
+    }
+
+    /**
+     * 外部ストレージにある画像一覧を表示する。
+     * ユーザはその中から画像を一つ選び、選んだ画像がEditPanelに表示される（まだsaveはされない）
+     * 外部ストレージにアクセスする許可がなければ、許可を求める。
+     * ユーザーが拒否すれば、このアプリで画像は扱えない。
+     */
+    private fun showPicturesFromExternalStorage() {
+        // Android 6, API 23以上でパーミッションの確認が必要
+        // https://akira-watson.com/android/external-storage-image.html
+        if (Build.VERSION.SDK_INT >= 23) {
+            val permission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            if (permission == PackageManager.PERMISSION_GRANTED) {
+                // すでに許可されているなら、ExternalStorageから画像を選ばせる
+                showPictureDialog()
+            } else {
+                // 許可されていなければ、許可を求める
+                requestLocationPermission()
+            }
+        } else {
+            // 23より下のAPIならパーミッションチェックしないでExternalStorageから画像を選ばせる
+            showPictureDialog()
+        }
+    }
+
+    /**
+     * setResultで必要なデータを渡しておけば起動元のアクティビティのonActivityResultでそれを受け取ることができる。
+     */
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // RESULT_OK = -1, RESULT_CANCELED = 0
+        if (resultCode == Activity.RESULT_CANCELED) {
+            Toast.makeText(this, getString(R.string.activity_canceled), Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (data != null && requestCode == GALLERY) {
+            val contentURI = data.data
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
+                val bitmapDrawable = BitmapDrawable(resources, bitmap)
+                Toast.makeText(this, getString(R.string.image_not_saved_yet), Toast.LENGTH_SHORT).show()
+                edit_page_layout.background = bitmapDrawable
+
+            } catch (e: IOException) {
+                e.printStackTrace()
+                Toast.makeText(this, getString(R.string.failed_get_image), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    // 許可を求める
+    fun requestLocationPermission() {
+        // shouldShowRequestPermissionRationale():
+        // アプリがパーミッションを既にリクエストしていて、ユーザーがそのパーミッションを拒否した場合、このメソッドは true を返します。
+        // https://developer.android.com/training/permissions/requesting
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_CONTACTS),
+                    READ_PERMISSION_REQUEST_CODE)
+        } else {
+            val toast = Toast.makeText(this, getString(R.string.request_permission_msg), Toast.LENGTH_SHORT)
+            toast.show()
+
+            ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_CONTACTS),
+                    READ_PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    /**
+     * パーミッションを求めるダイアログにユーザーが応答すると、システムがこのメソッドを呼び出す。
+     * コールバックにはrequestPermission()に渡されたものと同じリクエストコードが渡される。
+     */
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            READ_PERMISSION_REQUEST_CODE -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // リクエストが許可された
+                // TODO: EditDiaryActivity::readImageFromExternalStorage()とすれば、
+                // requestLocationPermission()とonRequestPermissionsResult()はMyAppCompatActivityに移せる？
+                showPictureDialog()
+            } else {
+                // リクエストが拒否された
+                val toast = Toast.makeText(this, getString(R.string.permission_denied_msg), Toast.LENGTH_SHORT)
+                toast.show()
+            }
         }
     }
 
@@ -94,6 +206,7 @@ class EditDiaryActivity : MyAppCompatActivity() {
 
     /**
      * 日記をsaveする
+     * 画像もここでsaveする
      * UpdateかInsertかはDiaryのEntityがnullかどうかで判断
      */
     fun saveDiary() {
--- app/src/main/java/com/example/kouhei/todidlist/MyAppCompatActivity.kt
+++ app/src/main/java/com/example/kouhei/todidlist/MyAppCompatActivity.kt
@@ -1,11 +1,44 @@
 package com.example.kouhei.todidlist
 
-import android.content.Context
 import android.content.Intent
+import android.os.Environment
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.app.AlertDialog
 
 open class MyAppCompatActivity: AppCompatActivity() {
 
+    // 画像を置く外部ストレージのパスを設定
+    var filePath = Environment.getExternalStorageDirectory().path + "DCIM/Camera/" + R.string.neko_jpg_for_develop
+
+    val GALLERY = 1
+
+    /**
+     * https://demonuts.com/pick-image-gallery-camera-android/
+     */
+    fun showPictureDialog() {
+        val pictureDialog = AlertDialog.Builder(this)
+        pictureDialog.setTitle("Select Action")
+        // pictureDialogItemsの中から一つ選ぶToastが表示される
+        // 配列の二つ目に"use camera"を乗せて、takeCamera()を作り、setItems()でwhen 1 -> takeCamera()みたいにすれば、
+        // 写真を選ぶ選択肢を追加できる。
+        val pictureDialogItems = arrayOf("Select photo from gallery")
+        pictureDialog.setItems(pictureDialogItems,
+                // 使用してない変数は"_"にrenameできるが、後で何なのかわからなくなるのでそのままにする。
+                { dialog, which ->
+                    choosePhotoFromGallery()
+                })
+        pictureDialog.show()
+    }
+
+    private fun choosePhotoFromGallery() {
+        // ACTION_PICK: データから項目を選択し、選択した項目を返す。
+        // https://developer.android.com/reference/android/content/Intent.html#ACTION_PICK
+        val galleryIntent = Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        // 起動したActivityの結果をonActivityResultでそれを受け取ることができる。
+        startActivityForResult(galleryIntent, GALLERY)
+    }
 
     /**
      * 他のActivityに遷移するメソッド
