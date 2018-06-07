1e908d2b3e47fbc8bc9c9e5c4033b2c4847b1edd
Kouhei Sano <ratatatta6@gmail.com>
2018-06-07 22:52:39 +0900

    ImageDaoの作成

---
+++ app/src/main/java/com/example/kouhei/todidlist/ImageDao.kt
@@ -0,0 +1,31 @@
+package com.example.kouhei.todidlist
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Delete
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+
+@Dao
+interface ImageDao {
+
+    /**
+     * 作成されたファイル名でimageエンティティをinsertする。
+     * updateはしない。
+     */
+    @Insert
+    fun insert(image: Image)
+
+    /**
+     * 日記のcalendar_dateで画像のEntityをすべて取得する。
+     * 今のところ(2018/6/7)日記と画像は１対１だが、１対多にする改修の可能性を見越して。
+     */
+    @Query("SELECT * FROM image WHERE calendar_date = :calendarDate")
+    fun getImagesWithCalendarDate(calendarDate: Int): List<Image>
+
+    /**
+     * 画像を一つ削除する。
+     * 日記の画像を変更するときに使う。
+     */
+    @Delete
+    fun delete(image: Image)
+}
