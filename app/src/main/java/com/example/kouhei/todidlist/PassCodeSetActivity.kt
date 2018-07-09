a7ae2d7e03c912149b70d71110423bfa1a2a4316
Kouhei Sano <ratatatta6@gmail.com>
2018-07-09 22:33:54 +0900

    PassCodeをセットするアクティビティのファイルを作成

---
+++ app/src/main/java/com/example/kouhei/todidlist/PassCodeSetActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kouhei.todidlist
+
+import android.os.Bundle
+
+class PassCodeSetActivity : MyAppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pass_code_set)
+    }
+}
