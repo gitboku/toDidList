df921e6d86ce9d8c32d3f682820891348b240f9e
Kouhei Sano <ratatatta6@gmail.com>
2018-07-13 15:35:39 +0900

    パスコードを使用するトグルがOnの時だけ、パスコードをセットするEditTextが表示される。

--- app/src/main/java/com/example/kouhei/todidlist/PassCodeSetActivity.kt
+++ app/src/main/java/com/example/kouhei/todidlist/PassCodeSetActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_pass_code_set.*
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import com.example.kouhei.todidlist.MyApplication.Companion.APP_NEED_PASSCODE
 
@@ -20,7 +21,9 @@ class PassCodeSetActivity : MyAppCompatActivity() {
         // パスコードを使うかどうかのトグルを初期化する。
         // init{}に配置したらretResource()でエラーが出る。
         val initData = getSharedPreferences(getString(R.string.preference_file_name), AppCompatActivity.MODE_PRIVATE)
-        lock_switch.isChecked = initData.getBoolean(APP_NEED_PASSCODE, false)
+        val initChecked = initData.getBoolean(APP_NEED_PASSCODE, false)
+        lock_switch.isChecked = initChecked
+        setVisibility(initChecked)
 
         // トグルが押下された（パスコードを使うかどうかを変更した）ら、その情報をPreferenceに保存する。
         lock_switch.setOnCheckedChangeListener { buttonView, isChecked ->
@@ -28,6 +31,8 @@ class PassCodeSetActivity : MyAppCompatActivity() {
             val editor = data.edit()
             editor.putBoolean(APP_NEED_PASSCODE, isChecked)
             editor.apply()
+
+            setVisibility(isChecked)
         }
 
         // パスコードを入力パネルのソフトウェアキーボードの「決定」ボタンを押したときの操作。
@@ -43,7 +48,18 @@ class PassCodeSetActivity : MyAppCompatActivity() {
         }
     }
 
+    /**
+     * Viewの要素が見えるかどうか(visibility)をセットする。
+     */
+    fun setVisibility(isChecked: Boolean) {
+        val visibility = if (isChecked) View.VISIBLE else View.INVISIBLE
+        navigate_message.visibility = visibility
+        passcode_panel.visibility = visibility
+    }
 
+    /**
+     * Toolbarのボタンが押されたときに呼ばれるメソッド
+     */
     override fun onOptionsItemSelected(item: MenuItem) = when(item.itemId) {
         // 戻るボタン
         // ※R.id.homeは自分が作ったものなので反応しない。android.R.id.homeはAndroid SDKのもの
